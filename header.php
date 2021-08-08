831e5c83dc8cf75677eeb5d91832ac6f04c1967e
Jacco Mol <jacco@infi.nl>
2021-08-08 16:59:24 +0200

    Move menu out of header

--- header.php
+++ header.php
@@ -40,16 +40,16 @@
 				<?php
 			endif; ?>
 		</div><!-- .site-branding -->
+	</header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation">
+    <nav id="site-navigation" class="side-navigation">
         <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'villamunkfors' ); ?></button>
         <?php
         wp_nav_menu(
             array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
+                'theme_location' => 'menu-1',
+                'menu_id'        => 'primary-menu',
             )
         );
         ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    </nav>
