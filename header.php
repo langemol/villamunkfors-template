b9accddd9116b0c62dbe2542b7ffe197fec5d28e
Jacco Mol <jacco@infi.nl>
2021-08-13 16:31:23 +0200

    Add font for site-title

--- header.php
+++ header.php
@@ -17,6 +17,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
