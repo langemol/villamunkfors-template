0b0c6f5269f5fd6dab83115e329b03bd15bfa5cf
Jacco Mol <delangemol@gmail.com>
2022-12-22 15:39:31 +0100

    unknown changes

--- functions.php
+++ functions.php
@@ -141,6 +141,7 @@ add_action( 'widgets_init', 'villamunkfors_widgets_init' );
  */
 function villamunkfors_scripts() {
 	wp_enqueue_style( 'villamunkfors-style', get_stylesheet_uri(), array(), _S_VERSION );
+//	wp_enqueue_style( 'villamunkfors-font', get_template_directory_uri().'/fonts/IndieFlower-Regular.ttf', array(), _S_VERSION );
 	wp_style_add_data( 'villamunkfors-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'villamunkfors-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
